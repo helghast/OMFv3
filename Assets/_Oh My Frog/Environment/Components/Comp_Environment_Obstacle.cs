dcd4cb2603094b6a9c4a68b32cf86e4513049f56
dosuna7 <dosuna7@gmail.com>
2015-02-05 10:01:22 +0100

    Commit before merge

--- Assets/_Oh My Frog/Environment/Components/Comp_Environment_Obstacle.cs
+++ Assets/_Oh My Frog/Environment/Components/Comp_Environment_Obstacle.cs
@@ -7,12 +7,19 @@ public class Comp_Environment_Obstacle : MonoBehaviour
     public float speed;
     public float y;
     public Transform environmentTransform;
-    public Transform poolTransform;
+    private Transform injectTransform;
+    private Transform poolTransform;
 
     void Awake()
     {
         environmentTransform = GameObject.Find("EnvironmentScene").transform;
+        injectTransform = GameObject.Find("EnvironmentSpawnPoint").GetComponent<Transform>();
         poolTransform = GameObject.Find("Pool_Obstacles").transform;
+    }
+
+    void Start()
+    {
+        speed = EnvironmentManager.Instance.SpeedLayer9;
     }
 
     // Update is called once per frame
@@ -20,6 +27,17 @@ public class Comp_Environment_Obstacle : MonoBehaviour
     {
         Vector3 mov = Vector3.right * speed * Time.deltaTime;
         transform.Translate(mov, Space.World);
+    }
+
+    public void spawn()
+    {
+        if (!this.gameObject.activeSelf)
+        {
+            //this.GetComponentInChildren<ParticleEmitter>().renderer.sortingLayerName = "Particulas";
+            this.gameObject.SetActive(true);
+            this.transform.position = new Vector3(injectTransform.position.x, y, EnvironmentManager.Instance.ZLayer9);
+            this.transform.parent = this.environmentTransform;
+        }
     }
 
 
