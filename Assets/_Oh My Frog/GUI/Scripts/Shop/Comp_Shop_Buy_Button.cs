74293aecaf9458bec7fcbef0abdde51aaccaac80
helghast <helghast.elite.trooper@gmail.com>
2015-02-05 09:40:59 +0100

    jacobo: rehaciendo shop gui

---
+++ Assets/_Oh My Frog/GUI/Scripts/Shop/Comp_Shop_Buy_Button.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Comp_Shop_Buy_Button : MonoBehaviour
+{
+
+    private string[] status = new string[] { "buy", "confirm", "equip", "unequip" };
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
